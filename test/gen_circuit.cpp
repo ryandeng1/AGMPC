0873ae5b771108f10a80002b56692e1b90066f0c
Ryan <ryan.deng@gmail.com>
2018-10-21 22:14:25 +0000

    Starting to generate circuit

--- test/gen_circuit.cpp
+++ test/gen_circuit.cpp
@@ -1,4 +1,6 @@
 #include "emp-tool/emp-tool.h"
+
+//#include "emp-tool/circuits/float_circuit.h"
 using namespace emp;
 void ham(int n) {
 	Integer a(n, 0, ALICE);
@@ -32,11 +34,52 @@ void sort(int n) {
 	sort(A, n);
 	for(int i = 0; i < n; ++i)
 		A[i].reveal<string>();
+
 }
+
+/*
+vector<Float> matrix_mul(vector<Float> left_matrix, vector<Float> right_matrix, int dim) {
+	vector<Float> result(dim * dim);
+	for (int i = 0; i < dim * dim; i++) {
+		result[i] = Float(40, 20, 0);
+	}
+	for (int i = 0; i < dim; i++) {
+		for (int j = 0; j < dim; j++) {
+			for (int k = 0; k < dim; k++) {
+				result[i * dim + j] = result[i * dim + j] + left_matrix[i * dim + k] * right_matrix[k * dim + j];
+			
+			}
+		}
+	}
+	return result;
+
+} 
+*/
+Float matrix_mul(Float left_matrix, Float right_matrix, int dim) {
+	return Float(40, 20, 5);
+}
+
 int main(int argc, char** argv) {
-	setup_plain_prot(true, "sort.txt");
-	sort(128);	
+//	setup_plain_prot(true, "sort.txt");
+//	sort(128);	
 //	mult(2048);
 //	ham(1<<10);
-	finalize_plain_prot ();
-}
+//	finalize_plain_prot ();
+	int dim = 3;
+//	vector<Float> left_matrix(dim * dim);
+//	vector<Float> right_matrix(dim * dim);
+//	for (int i = 0; i < dim * dim; i++) {
+//		left_matrix[i] = Float(40, 20, i);
+//		right_matrix[i] = Float(40, 20, i);
+//	}
+	/*
+	vector<Float> result = matrix_mul(left_matrix, right_matrix, dim);
+	for (int i = 0; i < dim * dim; i++) {
+		result[i].reveal<double>();
+	}
+	*/
+//	vector<emp::Float> hi(5);
+//	Float result = matrix_mul(Float(40, 20, 10), Float(40, 20, .54), 3);
+	sort(128);
+	finalize_plain_prot();
+}	
