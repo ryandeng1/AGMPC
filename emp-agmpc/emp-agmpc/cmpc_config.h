80d8cd30ad525d8b2d3c3bf69ba099877cf12579
Ryan Deng <denghe_i@didichuxing.com>
2018-11-20 16:13:19 -0800

    Changed config file to work with multiple machines

--- emp-agmpc/emp-agmpc/cmpc_config.h
+++ emp-agmpc/emp-agmpc/cmpc_config.h
@@ -2,7 +2,7 @@
 #define __CMPC_CONFIG
 const static int abit_block_size = 1024;
 const static int fpre_threads = 1;
-#define LOCALHOST
+//#define LOCALHOST
 //#define __MORE_FLUSH
 //#define __debug
 /*
@@ -12,9 +12,10 @@ const static char *IP[] = {""
 ,	"127.0.0.1"};
 */
 const static char *IP[] = {"ec2-52-39-162-238.us-west-2.compute.amazonaws.com", 
-"ec2-34-223-215-198.us-west-2.compute.amazonaws.com", 
-"ec2-23-20-124-131.compute-1.amazonaws.com", 
-"ec2-52-73-142-253.compute-1.amazonaws.com"};
+"ec2-34-223-215-198.us-west-2.compute.amazonaws.com"};
+
+//"ec2-23-20-124-131.compute-1.amazonaws.com", 
+//"ec2-52-73-142-253.compute-1.amazonaws.com"};
 
 const static bool lan_network = false;
 #endif// __C2PC_CONFIG
